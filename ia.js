db172aab5d0a1f5d590a82e79db093eb04373fd6
vavans <vannnns@gmail.com>
2014-03-26 00:09:55 +0100

    add endgame

--- ia.js
+++ ia.js
@@ -56,81 +56,126 @@ var colAccessorFactory = function(way) {
     }
 }; 
 
+var game = function() {
+    
+};
+
+var board = function(array) {
+    this.g = array;
+    this.score = 0;
+};
+
+board.prototype.equals = function(other) {
+    return other.scrore == this.score;
+};
+
+board.prototype.clone = function() {
+    var b = new board(new Array(	
+	new Array(0, 0, 0, 0),
+	new Array(0, 0, 0, 0),
+	new Array(0, 0, 0, 0),
+	new Array(0, 0, 0, 0)));
+    for (var x = 0; x < 4; x++) {
+	for (var y = 0; y < 4; y++) {
+	    b.g[x][y] = this.g[x][y];
+	}	
+    }
+    return b;
+};
 
 var grid = function () {
-    this.g = new Array(	
+    this.board = new board(new Array(	
 	new Array(2, 2, 2, 2),
 	new Array(4, 0, 4, 4),
 	new Array(2, 2, 0, 8),
-	new Array(4, 0, 2, 2));
+	new Array(4, 0, 2, 2)));
+};
 
-    this.score = 0;
-
-    this.moveCol = function(colAccessor) {	
-	var canGoDeeper = function(c, idx, value, min_y) {
-	    return (idx >= min_y && (c.get(idx) === 0 || (c.get(idx) === value)));  
-	};
-
-	var miny = 0;
-	for (y = 1; y < 4; y++) {	   
-	    var v = colAccessor.get(y);
-	    if (v === 0)
-		continue;
-	    colAccessor.set(y,0);
-	    
-	    var ny = y - 1;
-
-	    var destination = y;
-
-	    while(canGoDeeper(colAccessor, ny, v, miny)) {
-		destination = ny;
-		if (colAccessor.get(ny) === v) {
-		    colAccessor.set(ny,0);
-		    v *= 2;
-		    this.score += v;
-		    miny = ny + 1;
-		}
-		ny--;
-	    }
-	    colAccessor.set(destination, v);
-	}
+grid.prototype.moveCol = function(colAccessor) {	
+    var canGoDeeper = function(c, idx, value, min_y) {
+	return (idx >= min_y && (c.get(idx) === 0 || (c.get(idx) === value)));  
     };
-
-    this.play = function(way) {
-	var x,y = 0;
-	for (x = 0; x < 4; x++) {	
-	    this.moveCol(colAccessorFactory(way)(this.g, x));
-	}
-    };
-
-    this.isOver = function() {
+    
+    var miny = 0;
+    for (y = 1; y < 4; y++) {	   
+	var v = colAccessor.get(y);
+	if (v === 0)
+	    continue;
+	colAccessor.set(y,0);
 	
-    };
+	var ny = y - 1;
+	
+	var destination = y;
+	
+	while(canGoDeeper(colAccessor, ny, v, miny)) {
+	    destination = ny;
+	    if (colAccessor.get(ny) === v) {
+		colAccessor.set(ny,0);
+		v *= 2;
+		this.board.score += v;
+		miny = ny + 1;
+	    }
+	    ny--;
+	}
+	colAccessor.set(destination, v);
+    }
+};
+
+grid.prototype.play = function(way) {
+    var x = 0;
+    for (x = 0; x < 4; x++) {	
+	this.moveCol(colAccessorFactory(way)(this.board.g, x));
+    }
+};
+
+grid.prototype.isOver = function() {
+    var newGrid = new grid();
+    newGrid.board = this.board.clone();
+    for (var way = 1; way < 5; way++) {
+	for (x = 0; x < 4; x++) {		    
+	    newGrid.moveCol(colAccessorFactory(way)(newGrid.board.g, x));
+	    if (!newGrid.board.equals(this.board)) {
+		return false;
+	    }
+	}
+    }
+    return true;
 };
 
 
 
-(function () {
+
+
+var ia = (function () {
     var p2 = 0.6;
     var p4 = 0.4;
 
-    
+    return {
+	getMove : function(board) {
+	    return Math.floor(Math.random()*4) + 1;
+	}
+    };
 })();
 
 
 function dumpGrid(grid) {
     var html = '<ul style="display: inline-block">';
-    html += "<li>score: " + grid.score  + "</li>";
+    html += "<li>over: " + grid.isOver()  + ". score: " + grid.board.score  + "</li>";
     for (var y = 3; y >= 0; y--) {
 	html += "<li>";
 	for (var x = 0; x < 4; x++) {
-	    html += grid.g[x][y] + " ";
+	    html += grid.board.g[x][y] + " ";
 	}    
 	html += "</li>";	
     }
     html += "</ul>";
     return html;
 }
+
+var simulator = function(ia) {
+    
+    while 
+};
 
 var g = new grid();
 document.write("initial state <br/>");
